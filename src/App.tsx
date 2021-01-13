d2e005c4768e3db9dddcb968775fb9552e9b42b4
Daniel Figueroa <danfigueroa81@gmail.com>
2021-01-12 22:06:08 -0500

    Initial Commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+"use strict";
+
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import SearchBar from "./SearchBar";
+
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const updateInput = async (text: string) => {
+    setInput(text);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>Shoppies</header>
+      <SearchBar input={input} onChange={updateInput} />
     </div>
   );
 }
---
+++ src/SearchBar.tsx
@@ -0,0 +1,26 @@
+"use strict";
+
+interface IProps {
+  input: string;
+  onChange: (text: string) => void;
+}
+
+const SearchBar = ({ input, onChange }: IProps) => {
+  const BarStyling = {
+    width: "20rem",
+    background: "#F2F1F9",
+    border: "none",
+    padding: "0.5rem",
+  };
+  return (
+    <input
+      style={BarStyling}
+      key="random1"
+      value={input}
+      placeholder={"Search Movies"}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
+};
+
+export default SearchBar;
